8db38c879ce48105f1b1757e391aee5b023df6b2
Nicole Gilbert <nicolegilbert16@gmail.com>
2018-01-26 09:47:15 -0800

    added psudocode for the parentheses drill

--- algorithms.js
+++ algorithms.js
@@ -6,7 +6,6 @@ const Stack = require('./stack');
 
 const palinDromes = str => {
   str = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
-  let newStr = '';
 
   let strStack = new Stack();
 
@@ -14,6 +13,10 @@ const palinDromes = str => {
     strStack.push(str[i]);
   }
 
+  //For loop that goes to half of the string length-then map what we're
+  //popping to the str[0]
+  
+  let newStr = '';
   while (strStack.top !== null) {
     newStr += strStack.pop();
   }
@@ -26,3 +29,13 @@ console.log(palinDromes('dad'));
 console.log(palinDromes('1001'));
 console.log(palinDromes('Tauhida'));
 
+
+//== MATCHING PARENTHESES ==//
+
+//Example: (1+2)+3
+//Example 2: )1+2)+3
+
+//Check that for every open parentheses we have a close parentheses
+//If I see an open parentheses I will push only the open parentheses to the stack
+//If I see a closed parentheses I'll pop (only open parenthese will be available to pop) everything out of the stack
+//If stack is empty, parentheses are valid
