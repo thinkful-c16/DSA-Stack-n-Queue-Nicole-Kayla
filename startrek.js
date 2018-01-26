7c3d60fa7ff5993783fa8cf3a9219cbd9db84ec3
Kayla Webb <kaylarwebb@gmail.com>
2018-01-26 11:47:10 -0500

    Add helper for displaying the stack

--- startrek.js
+++ startrek.js
@@ -10,7 +10,7 @@ function createStack(){
   starTrek.push('Spock');
   starTrek.push('McCoy');
   starTrek.push('Scotty');
-  console.log(JSON.stringify(starTrek, null, 2));
+  // console.log(JSON.stringify(starTrek, null, 2));
 }
 
 createStack();
@@ -18,8 +18,13 @@ createStack();
 const helpers = {
   peek: function(stack) {
     return stack.top.data;
+  },
+
+  display: function(stack) {
+    console.log(JSON.stringify(stack, null, 2));
   }
 };
 
 
-console.log(helpers.peek(starTrek));
+// console.log(helpers.peek(starTrek));
+helpers.display(starTrek);
