dcbfa27741762f1b707d02f34069d456241e7d1e
Kayla Webb <kaylarwebb@gmail.com>
2018-01-26 11:20:24 -0500

    Create node class and add push method to stack class

---
+++ stack.js
@@ -0,0 +1,28 @@
+'use strict';
+
+class _Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(data) {
+    if (this.top === null) {
+      this.top = new _Node(data);
+      return this.top;
+    }
+    const node = new _Node(data);
+    node.next = this.top;
+    this.top = node;
+  }
+
+  pop() {
+    ///code
+  }
+}
